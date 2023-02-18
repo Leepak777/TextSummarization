885d6a214fddba1b962a76a3227aff955aaffd93
KevinTDHua <89712757+KevinTDHua@users.noreply.github.com>
2023-02-18 15:45:59 -0800

    Add files via upload
    Updated inputWin

--- inputWin.py
+++ inputWin.py
@@ -2,8 +2,16 @@ import tkinter as tk
 
 def handle_input():
     input_str = input_entry.get()
-    output_label.config(text="You entered: " + input_str)
+    output_label.config(text="You entered: " + save_txt(input_str))
 
+def write_to_file(string_to_write, file_path):
+    with open(file_path, "w") as file:
+        file.write(string_to_write)
+        
+def save_txt():
+    input_str = input_entry.get()
+    write_to_file(input_str,"document.txt")
+    
 # create a new window
 window = tk.Tk()
 window.title("My App")
@@ -14,7 +22,7 @@ input_entry = tk.Entry(window)
 input_entry.pack()
 
 # create a button to handle the input
-button = tk.Button(window, text="Submit", command=handle_input)
+button = tk.Button(window, text="Submit", command=save_txt)
 button.pack()
 
 # create a label for output
